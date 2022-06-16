dc97df40b2eeb3d8c1829611a4aa4dca3ce9dd04
JiHyeon <bjh1540@naver.com>
2022-06-17 02:18:07 +0900

    connecting with GitHub

---
+++ app/src/main/java/com/main/namanalarm/AlarmReceiver.java
@@ -0,0 +1,22 @@
+package com.main.namanalarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent sIntent = new Intent(context, AlarmService.class);
+        sIntent.putExtra("state", intent.getStringExtra("State"));
+
+        // Oreo(26) 버전 이후부터는 Background 에서 실행을 금지하므로 Foreground 에서 실행해야 함!
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.0) {
+            context.startForegroundService(sIntent);
+        } else {
+            context.startService(sIntent);
+        }
+    }
+}
+
